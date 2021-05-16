016c7b8e38a00e14878d940222a54924d8dded32
Miloh <milton.avila@gmail.com>
2021-05-16 16:42:25 -0500

    commit 31

--- main_test.go
+++ main_test.go
@@ -54,9 +54,8 @@ func TestAddAccount(t *testing.T) {
 
 func TestAddTransaction(t *testing.T) {
 
-	testManagecard := managecard.NewCard()
-
 	t.Run("General review of the function", func(t *testing.T) {
+		testManagecard := managecard.NewCard()
 		transaction := managecard.Transaction{
 			Merchant: "",
 			Amount:   0,
@@ -81,9 +80,10 @@ func TestAddTransaction(t *testing.T) {
 		}
 	})
 
-	t.Run("Approval transaction", func(t *testing.T) {
+	t.Run("Test card-not-active", func(t *testing.T) {
+		testManagecard := managecard.NewCard()
 		account := managecard.Account{
-			ActiveCard:     true,
+			ActiveCard:     false,
 			AvailableLimit: 100,
 		}
 
@@ -105,12 +105,164 @@ func TestAddTransaction(t *testing.T) {
 		}
 		testManagecard.AddTransaction(transaction, &reply)
 
-		got := len(reply.Violations)
+		got := reply.Violations[0]
 
-		want := 1
+		want := "card-not-active"
 
 		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, reply)
+			t.Errorf("got %s want %s given, %v", got, want, reply)
+		}
+	})
+
+	t.Run("Test account-not-initialized", func(t *testing.T) {
+		testManagecard := managecard.NewCard()
+		account := managecard.Account{
+			ActiveCard:     true,
+			AvailableLimit: 100,
+		}
+
+		reply := managecard.DataOuputs{
+			Account:    account,
+			Violations: []string{},
+		}
+
+		transaction := managecard.Transaction{
+			Merchant: "t",
+			Amount:   10,
+			Time:     "2020-01-02T15:04:05.000Z",
+		}
+
+		reply = managecard.DataOuputs{
+			Account:    account,
+			Violations: []string{},
+		}
+		testManagecard.AddTransaction(transaction, &reply)
+
+		got := reply.Violations[0]
+
+		want := "account-not-initialized"
+
+		if got != want {
+			t.Errorf("got %s want %s given, %v", got, want, reply)
+		}
+	})
+
+	t.Run("Test insufficient-limit", func(t *testing.T) {
+		testManagecard := managecard.NewCard()
+		account := managecard.Account{
+			ActiveCard:     true,
+			AvailableLimit: 10,
+		}
+
+		reply := managecard.DataOuputs{
+			Account:    account,
+			Violations: []string{},
+		}
+
+		transaction := managecard.Transaction{
+			Merchant: "t",
+			Amount:   100,
+			Time:     "2020-01-02T15:04:05.000Z",
+		}
+
+		testManagecard.AddAccount(account, &reply)
+		reply = managecard.DataOuputs{
+			Account:    account,
+			Violations: []string{},
+		}
+		testManagecard.AddTransaction(transaction, &reply)
+
+		got := reply.Violations[1]
+
+		want := "insufficient-limit"
+
+		if got != want {
+			t.Errorf("got %s want %s given, %v", got, want, reply)
+		}
+	})
+
+	t.Run("Test doubled-transaction", func(t *testing.T) {
+		testManagecard := managecard.NewCard()
+		account := managecard.Account{
+			ActiveCard:     true,
+			AvailableLimit: 100,
+		}
+
+		reply := managecard.DataOuputs{
+			Account:    account,
+			Violations: []string{},
+		}
+
+		transaction := managecard.Transaction{
+			Merchant: "t",
+			Amount:   10,
+			Time:     "2020-01-02T15:04:05.000Z",
+		}
+
+		testManagecard.AddAccount(account, &reply)
+		testManagecard.AddTransaction(transaction, &reply)
+		reply = managecard.DataOuputs{
+			Account:    account,
+			Violations: []string{},
+		}
+		testManagecard.AddTransaction(transaction, &reply)
+
+		got := reply.Violations[1]
+
+		want := "doubled-transaction"
+
+		if got != want {
+			t.Errorf("got %s want %s given, %v", got, want, reply)
+		}
+	})
+
+	t.Run("Test high-frequency-small-interval", func(t *testing.T) {
+		testManagecard := managecard.NewCard()
+		account := managecard.Account{
+			ActiveCard:     true,
+			AvailableLimit: 100,
+		}
+
+		reply := managecard.DataOuputs{
+			Account:    account,
+			Violations: []string{},
+		}
+
+		transaction := managecard.Transaction{
+			Merchant: "t",
+			Amount:   10,
+			Time:     "2020-01-02T15:04:05.000Z",
+		}
+
+		testManagecard.AddAccount(account, &reply)
+		testManagecard.AddTransaction(transaction, &reply)
+		reply = managecard.DataOuputs{
+			Account:    account,
+			Violations: []string{},
+		}
+		transaction = managecard.Transaction{
+			Merchant: "x",
+			Amount:   10,
+			Time:     "2020-01-02T15:04:05.000Z",
+		}
+		testManagecard.AddTransaction(transaction, &reply)
+		reply = managecard.DataOuputs{
+			Account:    account,
+			Violations: []string{},
+		}
+		transaction = managecard.Transaction{
+			Merchant: "y",
+			Amount:   10,
+			Time:     "2020-01-02T15:04:05.000Z",
+		}
+		testManagecard.AddTransaction(transaction, &reply)
+
+		got := reply.Violations[1]
+
+		want := "high-frequency-small-interval"
+
+		if got != want {
+			t.Errorf("got %s want %s given, %v", got, want, reply)
 		}
 	})
 
